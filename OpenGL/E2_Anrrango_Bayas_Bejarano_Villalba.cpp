7655ff3d3f275481247fe14bea0a377788132397
Santiago Bejarano <santiago.bejarano@epn.edu.ec>
2024-02-24 12:14:45 -0500

    Corregido erores, y uso de metodos para funcionalidades

--- OpenGL/E2_Anrrango_Bayas_Bejarano_Villalba.cpp
+++ OpenGL/E2_Anrrango_Bayas_Bejarano_Villalba.cpp
@@ -26,9 +26,13 @@ void drawM4(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& m4);
 void drawDeagle(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& deagle);
 void drawBayonet(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& bayonet);
 
-// settings
-const unsigned int SCR_WIDTH = 1920;
+// settings FHD
+const unsigned int SCR_WIDTH = 1920; 
 const unsigned int SCR_HEIGHT = 1080;
+
+// settings 2K
+//const unsigned int SCR_WIDTH = 2560;
+//const unsigned int SCR_HEIGHT = 1440;
 
 // Límites para el movimiento de la cámara en el mapa 100x100
 const float X_MIN_LIMIT = 0.0f;
@@ -55,7 +59,6 @@ bool showBayonet = true;
 int main()
 {
     // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -66,7 +69,6 @@ int main()
 #endif
 
     // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "", NULL, NULL);
     if (window == NULL)
     {
@@ -94,15 +96,12 @@ int main()
     //stbi_set_flip_vertically_on_load(true);
 
     // configure global opengl state
-    // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
     // build and compile shaders
-    // -------------------------
     Shader ourShader("shaders/shader_exercise16_mloading.vs", "shaders/shader_exercise16_mloading.fs");
 
     // load models
-    // -----------
     //Model ourModel(FileSystem::getPath("resources/objects/backpack/backpack.obj"));
     //Model ourModel("D:/LEONARDO/VisualStudio/OpenGL/OpenGL/model/baphomet/baphomet.obj");
     Model deagle("model/deagle/deagle.gltf");
@@ -113,7 +112,7 @@ int main()
     Model bayonet("model/bayonet/bayonet.gltf");
 
     float vertices[] = {
-        // Coordenadas XYZ         // Normales XYZ          // Coordenadas de textura UV
+       // Coordenadas XYZ         // Normales XYZ           // Coordenadas de textura UV
        // Cara trasera
        -0.6f, -0.6f, -0.6f,       0.0f,  0.0f, -1.0f,       0.5343f, 0.51f,    // Esquina inferior izquierda
         0.6f, -0.6f, -0.6f,       0.0f,  0.0f, -1.0f,       0.7291f, 0.3264f,    // Esquina inferior derecha    
@@ -163,11 +162,10 @@ int main()
         -0.6f,  0.6f, -0.6f,       0.0f,  1.0f,  0.0f,       0.5343f, 0.3264f  // Esquina superior izquierda  
     };
 
-    //SUELO
-    // Ajustar el tamaño de este array para contener 10,000 posiciones (100 filas de 100)
-
+    // SUELO PROVISIONAL
+    // Optimización para pruebas funcionales
+    // Falta modelo de isla
     glm::vec3 cubePositions[10000];
-    // Llenar el array con posiciones para crear 100 filas de 100 cubos cada una
     int index = 0; // Índice para llenar el arreglo
     for (int j = 0; j < 100; j++) { // 10 filas
         for (int i = 0; i < 100; i++) { // 10 cubos por fila
@@ -198,7 +196,6 @@ int main()
     unsigned int diffuseMap1 = loadTexture("textures/concreto.jpg");
 
     // shader configuration
-    // --------------------
     ourShader.use();
     ourShader.setInt("material.diffuse1", 0); // Texture unit 0
     
@@ -208,29 +205,24 @@ int main()
     camera.MovementSpeed = 7; //Optional. Modify the speed of the camera
 
     // render loop
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
         // per-frame time logic
-        // --------------------
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
         // input
-        // -----
         processInput(window);
 
         // render
-        // ------
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // don't forget to enable shader before setting uniforms
         ourShader.use();
         glActiveTexture(GL_TEXTURE0); // Cambia a la unidad de textura de los cubos
-        glBindTexture(GL_TEXTURE_2D, diffuseMap1);
-        
+        glBindTexture(GL_TEXTURE_2D, diffuseMap1);       
 
         // view / projection / transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1500.0f);
@@ -240,66 +232,53 @@ int main()
 
         glBindVertexArray(cubeVAO);
 
-        // Modificar el bucle de dibujo para iterar sobre los 10,000 cubos
+        // Dibujar 10.000 cubos, para 
         for (unsigned int i = 0; i < 10000; i++) {
             // Transformación del mundo para este cubo
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             ourShader.setMat4("model", model);
-
             // Dibuja el cubo
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
 
         //TARGET
-        /*float angle = 90.0f;
-        glm::mat4 model = glm::mat4(1.0f);
-        //model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it downada
-        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.0f, 0.0f));
-
-        ourShader.setMat4("model", model);
-        targetModel.Draw(ourShader);*/
+        glm::mat4 targetMatrix = glm::mat4(1.0f);
+        targetMatrix = glm::translate(targetMatrix, glm::vec3(10.0f, 2.0f, 50.0f));
+        targetMatrix = glm::rotate(targetMatrix, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        targetMatrix = glm::rotate(targetMatrix, glm::radians(-90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+        targetMatrix = glm::scale(targetMatrix, glm::vec3(0.2f, 0.2f, 0.2f));
+        ourShader.setMat4("model", targetMatrix);
+        target.Draw(ourShader);
 
         // SKYBOX
-        glm::mat4 skyboxMatrix = glm::mat4(1.0f);
-        skyboxMatrix = glm::translate(skyboxMatrix, glm::vec3(50.0f, 0.0f, 50.0f));
-        skyboxMatrix = glm::rotate(skyboxMatrix, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        skyboxMatrix = glm::rotate(skyboxMatrix, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        skyboxMatrix = glm::scale(skyboxMatrix, glm::vec3(1000.0f));
-        ourShader.setMat4("model", skyboxMatrix);
-        skybox.Draw(ourShader);
+        drawSkybox(ourShader, view, projection, skybox);
 
         // Decidir qué arma dibujar
-        // DEAGLE
+        // Dibujar Deagle
         if (showDeagle) { 
             drawDeagle(ourShader, view, projection, deagle);
         }
-        // M4
+        // Dibujar M4
         else if (showM4) {
             drawM4(ourShader, view, projection, m4);
         }
-        // BAYONET
+        // Dibujar Bayonet
         else if (showBayonet) {
             drawBayonet(ourShader, view, projection, bayonet);
-        }
-
-                    
+        }                    
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -348,7 +327,6 @@ void processInput(GLFWwindow* window)
 
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
@@ -357,7 +335,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 // glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
     if (firstMouse)
@@ -377,7 +354,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
@@ -420,6 +396,7 @@ unsigned int loadTexture(char const* path)
     return textureID;
 }
 
+// Dibujar Deagle
 void drawDeagle(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& deagle) {
     glm::mat4 pistolaMatrix = glm::mat4(1.0f);
     pistolaMatrix = glm::translate(pistolaMatrix, glm::vec3(0.21f, -0.4f, -0.38f));
@@ -431,6 +408,7 @@ void drawDeagle(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& d
     deagle.Draw(shader);
 }
 
+// Dibujar M4
 void drawM4(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& m4) {
     glm::mat4 armaMatrix = glm::mat4(1.0f);
     armaMatrix = glm::translate(armaMatrix, glm::vec3(0.28f, -0.7f, -0.1f));
@@ -441,6 +419,7 @@ void drawM4(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& m4) {
     m4.Draw(shader);
 }
 
+// Dibujar Bayonet
 void drawBayonet(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& bayonet) {
     glm::mat4 cuchilloMatrix = glm::mat4(1.0f);
     cuchilloMatrix = glm::translate(cuchilloMatrix, glm::vec3(0.38f, -0.4f, -1.0f));
@@ -450,4 +429,15 @@ void drawBayonet(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& 
     cuchilloMatrix = glm::inverse(view) * cuchilloMatrix;
     shader.setMat4("model", cuchilloMatrix);
     bayonet.Draw(shader);
+}
+
+// Dibujar Skybox
+void drawSkybox(Shader& shader, glm::mat4& view, glm::mat4& projection, Model& skybox) {
+    glm::mat4 skyboxMatrix = glm::mat4(1.0f);
+    skyboxMatrix = glm::translate(skyboxMatrix, glm::vec3(50.0f, 0.0f, 50.0f));
+    skyboxMatrix = glm::rotate(skyboxMatrix, glm::radians(135.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    skyboxMatrix = glm::rotate(skyboxMatrix, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    skyboxMatrix = glm::scale(skyboxMatrix, glm::vec3(1000.0f));
+    shader.setMat4("model", skyboxMatrix);
+    skybox.Draw(shader);
 }
